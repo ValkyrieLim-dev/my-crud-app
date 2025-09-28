d50cc9cab7c9ff0a9d5d0c0a384f75ac4ba789a4
ValkyrieLim-dev <kennethalvinlim@gmail.com>
2025-09-28 22:53:54 +0800

    Add homepage

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+// src/app/page.tsx
+export default function Home() {
+  return (
+    <main>
+      <h1>Welcome to My CRUD App 🚀</h1>
+      <a href="/activity-log">Go to Activity Log</a>
+    </main>
+  )
+}
